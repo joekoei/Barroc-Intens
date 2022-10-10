f335cd5649e595552005b4c60edf8b10ba989c03
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-10 13:21:39 +0200

    Fixed homepage routing etc

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+//
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
