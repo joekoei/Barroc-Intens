4fd8a5837ce6e506e6ab099203f11135199cf368
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-10 13:06:26 +0200

    All migrations done.

---
+++ database/migrations/2022_10_10_105412_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string("phone");
+            $table->string("street");
+            $table->string("house_number");
+            $table->string('city');
+            $table->string('country_code');
+            $table->dateTime("bkr_checked");
+            $table->foreignId('contact_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
