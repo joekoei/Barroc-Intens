393c946e919b2d8cc3cd2c0fb94afb53aaa7cdb3
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-10 13:12:16 +0200

    Little bit of documentation

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -10,6 +10,8 @@ class Company extends Model
     use HasFactory;
 
 
+    /* In een view kan je hierdoor bij een bedrijf heel simpel de eigenaar verkrijgen, je doet simpelweg $company->owner->name (om de eigenaar zn naam te pakken) */
+
     public function owner(){
         return $this->hasOne(User::class,'id');
     }
