10d9b6e2fde8b069d7c6f4507e2f0c7556f59137
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-11 10:51:55 +0200

    Rerouted to controller

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [PagesController::class,'index'])->name('homepage');
 
 Route::get("/bkr",[PagesController::class,'bkr'])->name('bkr');
 
