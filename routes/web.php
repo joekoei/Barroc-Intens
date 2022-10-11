5031e749abacdf060aa17360115dfb79dc4a1b1b
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-11 10:26:26 +0200

    Fixed routing and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 });
+
+Route::get("/bkr",[PagesController::class,'bkr'])->name('bkr');
 
 Route::prefix("dashboard")->group(function (){
     Route::prefix("sales")->group(function () {
