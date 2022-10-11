9969aea85e5f132eec4d253fd70c6323af8f07e2
Tomponij <Tomslavenburg@gmail.com>
2022-10-11 10:55:25 +0200

    background image push

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.base')
+
+@section('content')
+    <!-- Background image -->
+    <div
+        class="bg-image"
+        style="
+    background-image: url('{{asset('img/banner-BG.jpg')}}');
+    height: 80vh; background-size: cover; background-position: center center">
+
+        <h1>test</h1>
+    </div>
+    <!-- Background image -->
+
+@endsection
