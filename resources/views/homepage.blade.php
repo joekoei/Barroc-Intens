81639a27ee388f7dcca0739b3f1fa2a4e4545d29
SuperTurk38 <d280587@edu.rocwb.nl>
2022-10-11 11:25:13 +0200

    Added img and section (Wie zijn wij)

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,14 +2,24 @@
 
 @section('content')
     <!-- Background image -->
-    <div
-        class="bg-image"
-        style="
-    background-image: url('{{asset('img/banner-BG.jpg')}}');
-    height: 80vh; background-size: cover; background-position: center center">
-
+    <div class="bg-image" style="background-image: url('{{asset('img/banner-BG.jpg')}}');height: 80vh; background-size: cover; background-position: center center">
         <h1>test</h1>
     </div>
     <!-- Background image -->
+
+    <!--  Wie zijn wij  -->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-sm">
+                <img src="{{asset('img/logo1_groot.png')}}" alt="wiezijnwij">
+            </div>
+            <div class="col-sm">
+                <h2>Wie zijn  wij?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, aut autem commodi, dolorum enim ex, laboriosam laborum minima modi molestias nobis repellendus repudiandae sequi similique sit temporibus veritatis. Voluptatem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, itaque minima! Ab illo libero molestias mollitia natus quis totam voluptatem? Commodi et eum ex excepturi labore officia porro reprehenderit tenetur.</p>
+            </div>
+        </div>
+    </div>
+    <!--  Wie zijn wij  -->
+
 
 @endsection
