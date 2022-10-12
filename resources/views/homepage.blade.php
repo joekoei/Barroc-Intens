3bc615f3fdcd7e063637a61c0dc825478fce585f
Tomponij <Tomslavenburg@gmail.com>
2022-10-12 13:33:20 +0200

    Over ons margin

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -44,15 +44,23 @@
 
     <!--  Wie zijn wij  -->
     <div class="container padding-bot-top">
-        <div class="row">
-            <div class="col">
-                <img src="{{asset('img/logo1_groot.png')}}" alt="wiezijnwij">
+        <div class="row db-margin">
+            <div class="col-sm-4">
+                <img class="db-imgwidth" src="{{asset('img/logo1_groot.png')}}" alt="wiezijnwij">
             </div>
-            <div class="col">
+            <div class="col-sm-8">
                 <h2>Wie zijn  wij?</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, aut autem commodi, dolorum enim ex, laboriosam laborum minima modi molestias nobis repellendus repudiandae sequi similique sit temporibus veritatis. Voluptatem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, itaque minima! Ab illo libero molestias mollitia natus quis totam voluptatem? Commodi et eum ex excepturi labore officia porro reprehenderit tenetur.</p>
             </div>
         </div>
+            <div class="row db-margin">
+                <div class="col-sm-8">
+                    <h2>Wie zijn  wij?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, aut autem commodi, dolorum enim ex, laboriosam laborum minima modi molestias nobis repellendus repudiandae sequi similique sit temporibus veritatis. Voluptatem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, itaque minima! Ab illo libero molestias mollitia natus quis totam voluptatem? Commodi et eum ex excepturi labore officia porro reprehenderit tenetur.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="db-imgwidth" src="{{asset('img/logo1_groot.png')}}" alt="wiezijnwij">
+                </div>
     </div>
     <!--  Wie zijn wij  -->
 
