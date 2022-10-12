7b09ef27f975f6f61a89a5524010ae0eeaa4f976
WesleyHaring <D284479@edu.rocwb.nl>
2022-10-12 10:20:54 +0200

    scollbar auth check

--- resources/views/parts/dashboard/scrollbar.blade.php
+++ resources/views/parts/dashboard/scrollbar.blade.php
@@ -18,25 +18,29 @@
                     </a>
                 </li>
             @endif
-            <li class="app-sidebar__heading" >Maintenance</li>
-            <li>
-                <a href="" id="agenda.side">
-                    <i class="metismenu-icon pe-7s-display2"></i>
-                    Agenda
-                </a>
-                <a href="" id="agenda.side">
-                    <i class="metismenu-icon pe-7s-display2"></i>
-                    Overzicht klanten
-                </a>
-            </li>
-            <li class="app-sidebar__heading">Inkoop</li>
-            <li>
-                <a href=""  id="klanten.side">
-                    <i class="metismenu-icon pe-7s-display2"></i>
-                    Producten
-                </a>
-            </li>
-
+            @if(Auth::user()->role()->name == "maintenance")
+                <li class="app-sidebar__heading" >Maintenance</li>
+                <li>
+                    <a href="" id="agenda.side">
+                        <i class="metismenu-icon pe-7s-display2"></i>
+                        Agenda
+                    </a>
+                    <a href="" id="agenda.side">
+                        <i class="metismenu-icon pe-7s-display2"></i>
+                        Overzicht klanten
+                    </a>
+                </li>
+            @endif
+            @if(Auth::user()->role()->name == "inkoop")
+                <li class="app-sidebar__heading">Inkoop</li>
+                <li>
+                    <a href=""  id="klanten.side">
+                        <i class="metismenu-icon pe-7s-display2"></i>
+                        Producten
+                    </a>
+                </li>
+            @endif
+            @if(Auth::user()->role()->name == "finance")
             <li class="app-sidebar__heading">Finance</li>
             <li>
                 <a href=""  id="factuur.side">
@@ -44,6 +48,7 @@
                     Facturen
                 </a>
             </li>
+            @endif
         </ul>
     </div>
 </div>
