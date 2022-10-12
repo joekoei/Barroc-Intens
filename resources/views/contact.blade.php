49a023cab9a237a4740c299631002205cb5130ef
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2022-10-12 11:08:37 +0200

    Added styling for contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="container">
+    <div class="bg-image row w-100 align-items-center">
+        <div class="align-middle">
+                <div class="container text-center">
+                    <h1 class="db-title db-color-yellow">Barroc Intens</h1>
+                    <p class="text-white fs-4">Het nummer 1 koffiemerk ter wereld</p>
+                    <div class="w-50 p-3 mx-auto d-flex justify-content-evenly db-margin-button">
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="container w-50">
     <form action="{{route('contact.store')}}" method="post">
         @csrf
         <div class="row">
+            <h1>Contact Formulier</h1>
             <div class="col-md-6">
                 <label class="form-label" for="emailInput">Email:</label>
                 <input class="form-control" type="text" value="" name="email" id="emailInput">
@@ -14,7 +25,7 @@
             </div>
             <br>
             <div class="col-md-6">
-                <label class="form-label" for="aanhefInput">Aanhef:</label>
+                <label  class="form-label" for="aanhefInput">Aanhef:</label>
                 <select class="form-control" id="aanhefInput" name="aanhef">
                     <option value="">Kies een aanhef</option>
                     <option value="Dhr.">Dhr.</option>
@@ -25,7 +36,7 @@
         <br>
         <div class="row">
             <div class="col-md-6">
-                <label for="firstNameInput">Voornaam:</label>
+                <label  for="firstNameInput">Voornaam:</label>
                 <input class="form-control" type="text" id="firstNameInput" name="firstname">
             </div>
             <br>
@@ -46,9 +57,9 @@
             <br>
             <textarea class="form-control" name="reasoncontact" id="" cols="30" rows="10"></textarea>
         </div>
+        <br>
         <div>
-            <input type="submit" class="btn btn-primary" value="Verzend">
+            <input type="submit" class="db-btn" value="Verzend">
         </div>
     </form>
-</div>
 @endsection
