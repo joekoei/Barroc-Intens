a87aef124454969360d1e0208faa06c3a3520d81
Tomponij <Tomslavenburg@gmail.com>
2022-10-13 21:51:42 +0200

    maintence table

--- resources/views/authenticated/maintenance/errors/index.blade.php
+++ resources/views/authenticated/maintenance/errors/index.blade.php
@@ -1,7 +1,32 @@
-@extends('layouts.base')
-
+@extends('layouts.auth')
 @section('content')
-<div class="container padding-bot-top">
-    <h1>Overzicht storingsmeldingen</h1>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="main-card mb-3 card">
+                <div class="card-header">Al onze standaard producten</div>
+                <div class="table-responsive">
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th class="text-center">Naam</th>
+                            <th class="text-center">Beschrijving</th>
+                            <th class="text-center">Prijs</th>
+                            <th class="text-center">Categorie</th>
+                            <th class="text-center">Laatst geupdate</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($appointment as $appointment)
+                            <tr>
+                                <td class="text-muted text-center">{{$appointment->company_id}}</td>
+                                <td class="text-muted text-center">{{$appointment->remark}}</td>
+                                <td class="text-muted text-center">{{$appointment->created_at}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
