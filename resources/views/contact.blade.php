2e6f750ab043a90ac87416abf9e55e3489a79b7a
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2022-10-13 15:30:46 +0200

    Changed styling for contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,29 +1,16 @@
 @extends('layouts.base')
+@include('parts.header')
 @section('content')
-    <div class="bg-image row w-100 align-items-center">
-        <div class="align-middle">
-                <div class="container text-center">
-                    <h1 class="db-title db-color-yellow">Barroc Intens</h1>
-                    <p class="text-white fs-4">Het nummer 1 koffiemerk ter wereld</p>
-                    <div class="w-50 p-3 mx-auto d-flex justify-content-evenly db-margin-button">
-                </div>
-            </div>
-        </div>
-    </div>
-<div class="container w-50">
+<div class="container w-50 padding-bot-top">
     <form action="{{route('contact.store')}}" method="post">
         @csrf
         <div class="row">
             <h1>Contact Formulier</h1>
-            <div class="col-md-6">
+            <div class="col-md-6 padding-bot">
                 <label class="form-label" for="emailInput">Email:</label>
                 <input class="form-control" type="text" value="" name="email" id="emailInput">
-                <div id="emailHelpBlock" class="form-text">
-                    Voer hier je email in.
-                </div>
             </div>
-            <br>
-            <div class="col-md-6">
+            <div class="col-md-6 padding-bot">
                 <label  class="form-label" for="aanhefInput">Aanhef:</label>
                 <select class="form-control" id="aanhefInput" name="aanhef">
                     <option value="">Kies een aanhef</option>
@@ -32,28 +19,24 @@
                 </select>
             </div>
         </div>
-        <br>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 padding-bot">
                 <label  for="firstNameInput">Voornaam:</label>
                 <input class="form-control" type="text" id="firstNameInput" name="firstname">
             </div>
-            <br>
-            <div class="col-md-6">
+            <div class="col-md-6 padding-bot">
                 <label for="lastNameInput">Achternaam:</label>
                 <input class="form-control" type="text" name="lastname" id="lastNameInput">
             </div>
         </div>
-        <br>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 padding-bot">
                 <label for="lastNameInput">Onderwerp:</label>
                 <input class="form-control" type="text" name="subject" id="lastNameInput">
             </div>
         </div>
-        <div class="col-12">
+        <div class="col-12 padding-bot">
             <label for="textBoxInput">Reden voor contact:</label>
-            <br>
             <textarea class="form-control" name="reasoncontact" id="" cols="30" rows="10"></textarea>
         </div>
         <br>
