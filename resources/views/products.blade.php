d8bc46c020fee618387f74e904729793b1fef60f
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-13 22:46:18 +0200

    Card fixes!!!!

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -11,10 +11,10 @@
             @foreach($products as $product)
                 <div class="col-md-4">
                     <div class="card" style="width: 18rem; margin: 10px;">
-                        <img src="{{$product->image_paths}}" class="card-img-top" alt="Picture" style="width: 18rem; height: 50%;">
+                        <img src="{{$product->image_paths}}" class="card-img-top" alt="Picture">
                         <div class="card-body">
                             <h5 class="card-title">{{$product->name}}</h5>
-                            <p class="card-text">{{$product->description}}</p>
+                            <p class="card-text" @if($product->cat()->id == 1) style="height: 30px;" @else style="height: 150px;" @endif>{{$product->description}}</p>
                             <a href="{{route('offers',$product->id)}}" class="btn btn-primary">Contact opnemen?</a>
                         </div>
                     </div>
