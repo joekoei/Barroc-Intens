a37704693b2d7a73de29ea7418b6f2ef95abc03d
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-13 21:26:47 +0200

    Company Seeder

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Company::create([
+            "name"=>"Devbyte",
+            "phone"=>"06123456789",
+            "street"=>"Teststraat",
+            "house_number"=>"14",
+            "city"=>"zevenbergen",
+            "country_code"=>"NL",
+            "bkr_checked"=>\DateTime::createFromFormat("y-m-d","2022-10-13")
+        ]);
+    }
+}
