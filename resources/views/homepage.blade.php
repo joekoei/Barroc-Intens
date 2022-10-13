f1b883e96e69af2635fb97dc0185f397daf67997
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-13 22:51:27 +0200

    Removed stashed in hmepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -18,15 +18,7 @@
                             @auth
                                 <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 db-link"><i class='bx bxs-user'></i>Dashboard</a>
                             @else
-<<<<<<< Updated upstream
-                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500">Log in</a>
-=======
                                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 db-link">Log in</a>
-
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">Register</a>
-                                @endif
->>>>>>> Stashed changes
                             @endauth
                         </div>
                     @endif
