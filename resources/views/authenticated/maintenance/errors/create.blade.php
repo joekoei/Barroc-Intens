00d52df80ef98debd4005e4e4b6c59820c22bde5
Tomponij <Tomslavenburg@gmail.com>
2022-10-18 12:51:03 +0200

    small fix

--- resources/views/authenticated/maintenance/errors/create.blade.php
+++ resources/views/authenticated/maintenance/errors/create.blade.php
@@ -1,24 +1,27 @@
-@extends('layouts.blade')
+@extends('layouts.base')
 
 @section('content')
-    <form>
-        <div class="form-group">
-            <label for="name">Remark</label>
-            <input type="text" class="form-control" id="remark">
-        </div>
-        <div class="form-group">
-            <label for="first">Bedrijf</label>
-            <select class="form-select" name="comp" id="comp">
-                <option selected>Choose...</option>
-                @foreach($companies as $comp)
-                    <option value="{{$comp->id}}">{{$comp->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label class="form-check-label" for="date">Enter appointment date</label>
-            <input type="date" class="form-select" id="date" name="date">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="name">Remark</label>
+                <input type="text" class="form-control" id="remark">
+            </div>
+            <div class="form-group">
+                <label for="first">Bedrijf</label>
+                <select class="form-select" name="comp" id="comp">
+                    <option selected>Choose...</option>
+                    @foreach($companies as $comp)
+                        <option value="{{$comp->id}}">{{$comp->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label class="form-check-label" for="date">Enter appointment date</label>
+                <input type="date" class="form-select" id="date" name="date">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
 @endsection
