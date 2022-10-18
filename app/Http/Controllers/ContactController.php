292a0b31c90049dc74fa3d8532f0abd5a1d7fe6d
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2022-10-18 10:54:38 +0200

    Added show to the ContactController.php and  added a sales dir with contactform.blade.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function index(Request $request){
+        $contact = ContactForm::all();
         return view('contact');
     }
     public function store(Request $request)
@@ -20,6 +21,12 @@ class ContactController extends Controller
             'content'=>json_encode($data)
         ]);
         return redirect()->route('products');
+    }
+
+    public function show($id){
+        $contact= ContactForm::findOrFail($id);
+        return view('authenticated.contact.see')->with(compact('contact'));
+
     }
 
     public function storeBKR(Request $request){
