09a5f6b52caf33c01b02969e111a3f476d9bccdc
Joey Blommers <joeyblommers2004@gmail.com>
2022-10-20 16:05:26 +0200

    Check nusret

--- resources/views/authenticated/maintenance/meetings/show.blade.php
+++ resources/views/authenticated/maintenance/meetings/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.auth')
 @section('content')
+    @php
+            $timestamp = $meeting->date_added;
+            $date = date('d-m-Y',strtotime($timestamp));
+            $time = date('H:i:s',strtotime($timestamp));
+    @endphp
 <div class="container">
     <h1>Informatie over  <strong class="text-yellow">{{$meeting->remark}}</strong></h1>
 
     <ul>
         <li>Afspraak naam <strong>{{$meeting->remark}}</strong></li>
-        <li>Afspraak is om  <strong>{{$meeting->date_added}}</strong></li>
+        <li>Afspraak is op <strong>{{$date}}</strong> om <strong>{{$time}}</strong></li>
     </ul>
 </div>
 @endsection
