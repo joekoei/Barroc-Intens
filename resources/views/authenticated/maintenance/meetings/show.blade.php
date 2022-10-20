e6cf34ad5a33efec65e1a6fd9c9953ab15603355
SuperTurk38 <d280587@edu.rocwb.nl>
2022-10-20 16:02:18 +0200

    Detailpage Appointements 2.0

--- resources/views/authenticated/maintenance/meetings/show.blade.php
+++ resources/views/authenticated/maintenance/meetings/show.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.auth')
 @section('content')
 <div class="container">
-    <h1>Informatie over <span class="text-yellow">{{$meeting->remark}}</span></h1>
+    <h1>Informatie over  <strong class="text-yellow">{{$meeting->remark}}</strong></h1>
 
     <ul>
-        <li>Afspraak naam {{$meeting->remark}}</li>
+        <li>Afspraak naam <strong>{{$meeting->remark}}</strong></li>
+        <li>Afspraak is om  <strong>{{$meeting->date_added}}</strong></li>
     </ul>
 </div>
 @endsection
