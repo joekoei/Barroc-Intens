ac7ce27021fa09c51be7ece3b7b0c4c6ca4207aa
Thomasbro312 <73693551+Thomasbro312@users.noreply.github.com>
2022-11-15 10:34:34 +0100

    Made a Leasecontract Seeder

---
+++ database/factories/LeasecontractFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\leasecontract;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Leasecontract>
+ */
+class LeasecontractFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Leasecontract::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "company_id"=>'1',
+            "agreed_rules"=>'Lorem ipsum dolor sit amet.',
+            "pay_method"=>'Maandelijks',
+            "prijs"=>'900',
+            "product"=>'Barroc Intens',
+            "status"=>'Betaald',
+            "firstname"=>$this->faker->firstname,
+            "lastname"=>$this->faker->lastname
+        ];
+    }
+}
